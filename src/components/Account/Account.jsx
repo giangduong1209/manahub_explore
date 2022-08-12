8c3fae8dc9922db0e0a3d646161b4f179b1feb35
vannguyen <ngkuka001@gmail.com>
2022-08-12 21:31:54 +0700

    update style

--- src/components/Account/Account.jsx
+++ src/components/Account/Account.jsx
@@ -1,53 +1,54 @@
-import { useMoralis } from "react-moralis";
-import { getEllipsisTxt } from "helpers/formatters";
-import Blockie from "../Blockie";
-import { Button, Card, Modal } from "antd";
-import { useState } from "react";
-import Address from "../Address/Address";
-import { SelectOutlined } from "@ant-design/icons";
-import { getExplorer } from "helpers/networks";
-import Text from "antd/lib/typography/Text";
-import { connectors, connectorsMobile } from "./config";
+import { useMoralis } from 'react-moralis';
+import { getEllipsisTxt } from 'helpers/formatters';
+import Blockie from '../Blockie';
+import { Button, Card, Modal } from 'antd';
+import { useState } from 'react';
+import Address from '../Address/Address';
+import { SelectOutlined } from '@ant-design/icons';
+import { getExplorer } from 'helpers/networks';
+import Text from 'antd/lib/typography/Text';
+import { connectors, connectorsMobile } from './config';
 import styles from './styles.module.css';
 import { WalletIcon } from '../MainHeader/IconHeader';
 
 const styles2 = {
   account: {
-    height: "42px",
-    padding: "0 15px",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    width: "fit-content",
-    borderRadius: "12px",
-    border: "solid 2px white",
-    cursor: "pointer",
+    height: '42px',
+    padding: '0 15px',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 'fit-content',
+    borderRadius: '12px',
+    border: 'solid 2px white',
+    cursor: 'pointer',
   },
   text: {
-    color: "#FEA013"
+    color: '#FFF',
   },
   connector: {
-    alignItems: "center",
-    display: "flex",
-    flexDirection: "column",
-    height: "auto",
-    justifyContent: "center",
-    marginLeft: "auto",
-    marginRight: "auto",
-    padding: "20px 5px",
-    cursor: "pointer",
+    alignItems: 'center',
+    display: 'flex',
+    flexDirection: 'column',
+    height: 'auto',
+    justifyContent: 'center',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    padding: '20px 5px',
+    cursor: 'pointer',
   },
   icon: {
-    alignSelf: "center",
-    fill: "rgb(40, 13, 95)",
-    flexShrink: "0",
-    marginBottom: "8px",
-    height: "30px",
+    alignSelf: 'center',
+    fill: 'rgb(40, 13, 95)',
+    flexShrink: '0',
+    marginBottom: '8px',
+    height: '30px',
   },
 };
 
 function Account() {
-  const { authenticate, isAuthenticated, account, chainId, logout } = useMoralis();
+  const { authenticate, isAuthenticated, account, chainId, logout } =
+    useMoralis();
   const [isModalVisible, setIsModalVisible] = useState(false);
   const [isAuthModalVisible, setIsAuthModalVisible] = useState(false);
 
@@ -65,35 +66,43 @@ function Account() {
           footer={null}
           onCancel={() => setIsAuthModalVisible(false)}
           bodyStyle={{
-            padding: "15px",
-            fontSize: "17px",
-            fontWeight: "500",
+            padding: '15px',
+            fontSize: '17px',
+            fontWeight: '500',
           }}
-          style={{ fontSize: "16px", fontWeight: "500" }}
+          style={{ fontSize: '16px', fontWeight: '500' }}
           width="340px"
         >
-          <div style={{ padding: "10px", display: "flex", justifyContent: "center", fontWeight: "700", fontSize: "20px" }}>
+          <div
+            style={{
+              padding: '10px',
+              display: 'flex',
+              justifyContent: 'center',
+              fontWeight: '700',
+              fontSize: '20px',
+            }}
+          >
             Connect Wallet
           </div>
-          <div style={{ display: "grid", gridTemplateColumns: "1fr 1fr" }}>
-              {connectors.map(({ title, icon, connectorId }, key) => (
-                <div
-                  style={styles2.connector}
-                  key={key}
-                  onClick={async () => {
-                    try {
-                      await authenticate({ provider: connectorId });
-                      window.localStorage.setItem("connectorId", connectorId);
-                      setIsAuthModalVisible(false);
-                    } catch (e) {
-                      console.error(e);
-                    }
-                  }}
-                >
-                  <img src={icon} alt={title} style={styles2.icon} />
-                  <Text style={{ fontSize: "14px" }}>{title}</Text>
-                </div>
-              ))}
+          <div style={{ display: 'grid', gridTemplateColumns: '1fr 1fr' }}>
+            {connectors.map(({ title, icon, connectorId }, key) => (
+              <div
+                style={styles2.connector}
+                key={key}
+                onClick={async () => {
+                  try {
+                    await authenticate({ provider: connectorId });
+                    window.localStorage.setItem('connectorId', connectorId);
+                    setIsAuthModalVisible(false);
+                  } catch (e) {
+                    console.error(e);
+                  }
+                }}
+              >
+                <img src={icon} alt={title} style={styles2.icon} />
+                <Text style={{ fontSize: '14px' }}>{title}</Text>
+              </div>
+            ))}
           </div>
         </Modal>
       </>
@@ -120,7 +129,9 @@ function Account() {
         Hi
       </button> */}
       <div style={styles2.account} onClick={() => setIsModalVisible(true)}>
-        <p style={{ marginRight: "5px", ...styles2.text }}>{getEllipsisTxt(account, 3)}</p>
+        <p style={{ marginRight: '5px', ...styles2.text }}>
+          {getEllipsisTxt(account, 3)}
+        </p>
         <Blockie currentWallet scale={3} />
       </div>
       <Modal
@@ -128,25 +139,34 @@ function Account() {
         footer={null}
         onCancel={() => setIsModalVisible(false)}
         bodyStyle={{
-          padding: "15px",
-          fontSize: "17px",
-          fontWeight: "500",
+          padding: '15px',
+          fontSize: '17px',
+          fontWeight: '500',
         }}
-        style={{ fontSize: "16px", fontWeight: "500" }}
+        style={{ fontSize: '16px', fontWeight: '500' }}
         width="400px"
       >
         Account
         <Card
           style={{
-            marginTop: "10px",
-            borderRadius: "1rem",
+            marginTop: '10px',
+            borderRadius: '1rem',
           }}
-          bodyStyle={{ padding: "15px" }}
+          bodyStyle={{ padding: '15px' }}
         >
-          <Address avatar="left" size={6} copyable style={{ fontSize: "20px" }} />
-          <div style={{ marginTop: "10px", padding: "0 10px" }}>
-            <a href={`${getExplorer(chainId)}/address/${account}`} target="_blank" rel="noreferrer">
-              <SelectOutlined style={{ marginRight: "5px" }} />
+          <Address
+            avatar="left"
+            size={6}
+            copyable
+            style={{ fontSize: '20px' }}
+          />
+          <div style={{ marginTop: '10px', padding: '0 10px' }}>
+            <a
+              href={`${getExplorer(chainId)}/address/${account}`}
+              target="_blank"
+              rel="noreferrer"
+            >
+              <SelectOutlined style={{ marginRight: '5px' }} />
               View on Explorer
             </a>
           </div>
@@ -154,17 +174,17 @@ function Account() {
         <Button
           size="large"
           style={{
-            width: "100%",
-            marginTop: "10px",
-            borderRadius: "0.5rem",
-            fontSize: "16px",
-            fontWeight: "500",
+            width: '100%',
+            marginTop: '10px',
+            borderRadius: '0.5rem',
+            fontSize: '16px',
+            fontWeight: '500',
             backgroundColor: '#FEA013',
-            color: 'white'
+            color: 'white',
           }}
           onClick={async () => {
             await logout();
-            window.localStorage.removeItem("connectorId");
+            window.localStorage.removeItem('connectorId');
             setIsModalVisible(false);
           }}
         >
