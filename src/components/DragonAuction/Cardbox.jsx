09800a4b373cf56a01d468ac7d54d894a929b824
Huynh Ba Huy <19127420@student.hcmus.edu.vn>
2022-08-31 10:04:12 +0700

    unhide countdown releasing nft time

--- src/components/DragonAuction/Cardbox.jsx
+++ src/components/DragonAuction/Cardbox.jsx
@@ -23,8 +23,6 @@ const Cardbox = () => {
 	if(Date.now() >= deadline) {
 		flag = true
 	}
-    // Hide countdown temporarily
-    flag = true;
     const [isNFTSale, setIsNFTSale] = useState(flag);
     const [format, setFormat] = useState(
         "D [days] H [hours] m [minutes] s [seconds]"
