f5a6b410bc84d290248d0bca9d17847020b48e29
Huynh Ba Huy <19127420@student.hcmus.edu.vn>
2022-09-27 16:11:17 +0700

    hide countdown

--- src/components/DragonAuction/Cardbox.jsx
+++ src/components/DragonAuction/Cardbox.jsx
@@ -25,7 +25,7 @@ const Cardbox = () => {
 	if(Date.now() >= deadline) {
 		flag = true
 	}
-    flag = true
+    // flag = true
     const [isNFTSale, setIsNFTSale] = useState(flag);
     const [format, setFormat] = useState(
         "D [days] H [hours] m [minutes] s [seconds]"
